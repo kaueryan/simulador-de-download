bf18b97843620ee01d9267bd97feb01f5fea0cc9
kaueryan <kaueryan0@gmail.com>
2026-03-20 17:49:38 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SimuladordeDownloads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread d1 = new Thread(() => Download("Arquivo 1"));
+            Thread d2 = new Thread(() => Download("Arquivo 2"));
+            Thread d3 = new Thread(() => Download("Arquivo 3"));
+
+            d1.Start();
+            d2.Start();
+            d3.Start();
+
+            d1.Join();
+            d2.Join();
+            d3.Join();
+
+        }
+
+        static void Download(string Arquivo)
+        {
+            for (int i = 0; i <= 100; i += 10)
+            {
+                Console.WriteLine($"{Arquivo}: {i}%");
+                Thread.Sleep(200);
+            }
+
+            Console.WriteLine($"{Arquivo} finalizado!");
+        }
+    }
+}
